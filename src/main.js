6bd680c4ca5de685629777e0022ee2efd53f47c4
Joshua Salcido <tavaresjoshua8@gmail.com>
2019-11-01 17:56:12 -0700

    Changes: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import store from './store'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import VueCookies from 'vue-cookies'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(VueCookies)
 
 new Vue({
 	router,
